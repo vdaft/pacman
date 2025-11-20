827912d3df1095c678fe5b25b5986f563e4d6751
Daft <voglauer.daniel@gmail.com>
2025-11-20 21:00:56 +0100

    v0.1.5 - Auth complete

--- readme.md
+++ readme.md
@@ -10,3 +10,4 @@ v0.1.1 - Add simple Movement and Physics for Pacman
 v0.1.2 - Loading Levels dynamically
 v0.1.3 - Better Level loading
 v0.1.4 - Swipe Detection & Auth start
+v0.1.5 - Auth complete
--- src/frontend/login/login.ts
+++ src/frontend/login/login.ts
@@ -1,15 +1,19 @@
+import {DEFAULT_URL} from "../../shared/url";
+
 async function start_oauth(): Promise<void> {
     console.log("Requesting Access!")
 
-    const x = await fetch("/api/access", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({})
-    })
+    const url = new URL(window.location.href);
 
-    console.log("Access requested!");
+    const state = url.searchParams.get("state");
+    const userId = url.searchParams.get("userId");
+
+    if (!state || !userId) {
+        window.location.href=`${DEFAULT_URL}/api/access`
+        return;
+    }
+
+    window.location.href=`${DEFAULT_URL}/api/login/?state=${state}&userId=${userId}`;
 }
 
 start_oauth();
