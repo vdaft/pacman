a860c89939dfd487121a0e35aa028441ae54c548
Daft <voglauer.daniel@gmail.com>
2025-11-21 14:28:31 +0100

    vite conf fix???

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    port: 5173,
     server: {
         proxy: {
             '/api': {
                 target: 'http://localhost:3000',
                 changeOrigin: true,
                 secure: false,
+                rewrite: (path) => path.replace(/^\/api/, '')
             }
         },
         host: true,
