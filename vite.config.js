1482934df4d291a776b705b5d7283aa0ce174f36
Daft <voglauer.daniel@gmail.com>
2025-11-21 14:23:49 +0100

    vite conf fix??

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
             }
         },
         host: true,
-        allowedOrigins: ['localhost', 'pe9015.schuelerprojekte.online']
-
+        allowedOrigins: ['localhost', 'pe9015.schuelerprojekte.online'],
+        allowedHosts: ['localhost', 'pe9015.schuelerprojekte.online']
     }
 });
